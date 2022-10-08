c56a4ee53aefaee974c5fb9918eb1c3ed9ba0c88
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-10-08 18:12:42 +0530

    try to add more repaly

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ import { Comments } from "./pages/Comments";
 
 function App() {
   const [data, setData] = useState([]);
-
+  console.log(data)
   useEffect(() => {
     axios.get("http://localhost:8080/posts").then((res) => {
-      // console.log(res.data);
       setData(res.data);
     });
   }, []);
-
+  
   return (
     <div className="App">
       <h2>Comments</h2>
       {data?.map((comment) => {
-        return <Comments key={comment.id} data={comment}></Comments>;
+        // console.log(comment)
+        return <Comments key={comment.id} data={comment} ></Comments>;
       })}
     </div>
   );
--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./style.css";
 
 export const Comments = ({ data }) => {
+  let [comment, setComment] = useState(data);
   const [visiable, setVisible] = useState(false);
-  const [showInputBox,setShowInputBox]=useState(false);
-  console.log(data);
+  const [showInputBox, setShowInputBox] = useState(false);
+  const [input, setInput] = useState("");
+
+
+  const addComment = () => {
+    let newdata = {
+      id: Date.now(),
+      author: data.author,
+      body: input,
+    };
+    if (!comment.replies) {
+      let updatedData = { ...comment, replies: [newdata] };
+      setComment(updatedData);
+      // return comment
+    } else {
+      let updatedData = { ...comment, replies: [...comment.replies, newdata] };
+
+      setComment(updatedData);
+      // return comment
+    }
+    console.log(comment);
+  };
   const expand = () => {
     setVisible(!visiable);
   };
-  console.log(data);
+
   return (
     <div style={{ padding: "10px" }}>
       <div className="mainReplayBox">
@@ -20,25 +42,37 @@ export const Comments = ({ data }) => {
         </button>
         <div className="replayBox">
           <div className="commentBody">
-            <span style={{ marginBottom: "0px" }}>{data.body}</span>
+            <span style={{ marginBottom: "0px" }}>{comment.body}</span>
 
             <div className="replayOption">
-              <span onClick={()=>{setShowInputBox(!showInputBox)}}>Replay</span>
+              <span
+                onClick={() => {
+                  setShowInputBox(!showInputBox);
+                }}
+              >
+                Replay
+              </span>
               <span>Give Award</span>
               <span>Share</span>
               <span>Report</span>
               <span>Save</span>
             </div>
-           {showInputBox && <div className="inputBox">
-              <input type="text" />
-              <button>Add Comment</button>
-            </div>}
-            
+            {showInputBox && (
+              <div className="inputBox">
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setInput(e.target.value);
+                  }}
+                />
+                <button onClick={addComment}>Add Comment</button>
+              </div>
+            )}
           </div>
 
           {visiable &&
-            data.replies &&
-            data.replies.map((replay) => {
+            comment.replies &&
+            comment.replies.map((replay) => {
               return (
                 <div style={{ padding: "10px" }} key={replay.id}>
                   <Comments data={replay}></Comments>
